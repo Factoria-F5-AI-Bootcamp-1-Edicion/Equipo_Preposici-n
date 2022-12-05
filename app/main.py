46bc5a9b8ae647250b266ec0c17d1852d2a7ce77
RaulMuM <rodriguezppp@gmail.com>
2022-12-05 11:25:47 +0100

    Borrador del proyecto NLP. Trabajamos con otro dataset para comprobar la metricas de nuestros modelos

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from routes.api_router import api_router
+#from config.openapi import tags_metadata
+
+app = FastAPI(
+    title="HateSpeech API",
+    description="a REST API using python to classify whether a comment or several comments are hateful or not",
+    version="0.0.1",
+    #openapi_tags=tags_metadata,
+)
+
+#Enrutamos
+app.include_router(api_router)
